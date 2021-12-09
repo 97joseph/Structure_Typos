7e2cd79a00486eca75d5de134b74735485bee9ce
jk <33089347+97joseph@users.noreply.github.com>
2021-12-09 19:11:50 +0300

    Initial commit

---
+++ L4.5-MaizeCobRobot/MaizeCobRobot.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace L4._5_MaizeCobRobot
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Maize Cob Counting Robot.");
+
+            const int maizeCobsPerMinute = 100;
+
+            //All the rest of your codes goes here
+            
+            
+        }
+    }
+}
